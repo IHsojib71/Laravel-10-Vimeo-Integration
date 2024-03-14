e2bc42a2f8369d975691bdbe76049d9250a3f776
Md Ismail Hossain <ihsojib43@gmail.com>
2024-03-14 11:37:21 +0600

    adding different operations functions

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -14,11 +14,10 @@ class VideosController extends Controller
             'title' => ['required', 'string', 'max:255'],
             'video' => ['required', 'file', 'mimes:mp4,mkv,3gb,avi,flv,webm,mov']
         ]);
-
-
-
+        //privacy only video can be watched from embedded ifram
         $vimeoVideoLink = Vimeo::upload($valid['video'], [
             'name' =>  $valid['title'],
+            'privacy' => ['view' => 'embed'],
             'folder' => '19872628',
         ]);
         $vimeoVideoId = explode('/videos/', $vimeoVideoLink)[1];
@@ -109,4 +108,58 @@ class VideosController extends Controller
             return false;
         }
     }
+    public static function uploadVideoToFolder(string $videoTitle, string $videoPath, string $folderID, string $privacyName)
+    {
+        $vimeoVideoLink = Vimeo::upload($videoPath, [
+            'name' =>  $videoTitle,
+            'privacy' => ['view' => $privacyName],
+            'folder' => $folderID,
+        ]);
+        $vimeoVideoId = explode('/videos/', $vimeoVideoLink)[1];
+        return $vimeoVideoId;
+    }
+
+    public static function uploadVideoToRoot(string $videoTitle, string $videoPath, string $privacyName)
+    {
+        $vimeoVideoLink = Vimeo::upload($videoPath, [
+            'name' =>  $videoTitle,
+            'privacy' => ['view' => $privacyName],
+        ]);
+        $vimeoVideoId = explode('/videos/', $vimeoVideoLink)[1];
+        return $vimeoVideoId;
+    }
+
+    public static function deleteFolderByID(string $folderID)
+    {
+        // Make a DELETE request to delete the folder
+        $response = Vimeo::request("/me/projects/{$folderID}", [], 'DELETE');
+
+        // Check if the request was successful
+        if ($response['status'] === 204) {
+            // Folder deleted successfully
+            return true;
+        } else {
+            // Handle errors
+            $errorMessage = $response['body']['error'];
+            // Handle error
+            return false;
+        }
+    }
+
+    public static function deleteFolderWithVideos(string $folderID)
+    {
+        // 1. Retrieve videos in the folder
+        $response = Vimeo::request("/me/projects/{$folderID}/videos", ['per_page' => 100], 'GET');
+        // Check if the request was successful
+        if ($response['status'] === 200) {
+            $videos = $response['body']['data'];
+            // 2. Delete each video
+            foreach ($videos as $video) {
+                $videoId = substr($video['uri'], strrpos($video['uri'], '/') + 1);
+                self::deleteSpecificVideo($videoId); // Assuming you have a method to delete a video
+            }
+        }
+    }
+
+
 }
